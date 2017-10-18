42ef0827c8a8bf214dedc2b0267cdf210baa5c06
Peter Ellis <peter.ellis2013nz@gmail.com>
2017-10-19 08:15:50 +1300

    improve explanation of R script in utils folder.

--- util/rename-files.R
+++ util/rename-files.R
@@ -1,5 +1,12 @@
 # util/rename-files.R
-# Script renames files from the original naming conv
+# Script renames files to remove the redundant first nine characters they all share,
+# and change suffixes from .txt to .sas so computers will understand they contain
+# SAS code.  This script is not needed for analysis in the IDI, it is just to help
+# with managing and presenting the code in the GitHub repository.
+#
+# Peter Ellis, 19 October 2017
+
+projdir <- setwd("tsy-chhd")
 
 files <- list.files()
 
@@ -7,3 +14,5 @@ new_names <- gsub("tsy-chhd-", "", files, fixed = TRUE)
 new_names <- gsub("txt$", "sas", new_names)
 
 file.rename(files, new_names)
+
+setwd(projdir)
